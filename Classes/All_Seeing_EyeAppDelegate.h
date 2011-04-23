3b30e928dc58ae97f47ae565b37413b86d5c2445
mrmekon <none@none>
2011-04-23 00:16:01 -0400

    Added blank project.

---
+++ Classes/All_Seeing_EyeAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  All_Seeing_EyeAppDelegate.h
+//  All-Seeing Eye
+//
+//  Created by Trevor Bentley on 4/22/11.
+//  Copyright 2011 trevorbentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface All_Seeing_EyeAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  All-Seeing Eye
+//
+//  Created by Trevor Bentley on 4/22/11.
+//  Copyright 2011 trevorbentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
