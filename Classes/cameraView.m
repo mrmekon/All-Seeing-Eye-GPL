5c524d7abb01a25a6bf8af289258f88b2e9e93df
mrmekon <none@none>
2011-04-26 15:56:56 -0400

    Top 1/3rd is camera image

--- Classes/cameraView.m
+++ Classes/cameraView.m
@@ -8,7 +8,6 @@
 
 #import "cameraView.h"
 
-
 @implementation cameraView
 
 @synthesize imageView;
@@ -19,12 +18,14 @@
 -(id)initWithFrame:(CGRect)aRect {
 	if (self = [super initWithFrame: aRect]) {
 		UIImage *wheelImage = [UIImage imageNamed: @"wheel-small.png"];
- 		CGRect screenBounds = [[UIScreen mainScreen] bounds];
- 		self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, -(((wheelImage.size.height) - screenBounds.size.height)/2), wheelImage.size.width, wheelImage.size.height)];
- 		self.imageView.image = wheelImage;
- 		self.imageView.userInteractionEnabled = YES;
+ 		CGRect screenBounds = [self bounds];
+ 		//self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, -(((wheelImage.size.height) - screenBounds.size.height)/2), wheelImage.size.width, wheelImage.size.height)];
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0,0.0, aRect.size.width,aRect.size.height)];
+ 		//self.imageView.image = wheelImage;
+ 		//self.imageView.userInteractionEnabled = YES;
  	  [self addSubview: self.imageView];
-    [self setBackgroundColor:[UIColor blackColor]];
+    //[self setBackgroundColor:[UIColor blackColor]];
+    [self setBackgroundColor:[UIColor whiteColor]];
     
     //AVCaptureSession *session = [[AVCaptureSession alloc] init];
     //AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession: session];
@@ -56,59 +57,67 @@
 	[self.captureSession addOutput:captureOutput];
 
 	self.cameraLayer = [CALayer layer];
-	self.cameraLayer.frame = [[UIScreen mainScreen] bounds];
-	self.cameraLayer.transform = CATransform3DRotate(CATransform3DIdentity, M_PI/2.0f, 0, 0, 1);
+  CGRect screenSize = [[UIScreen mainScreen] bounds];
+  CGRect rect = [self bounds];
+	self.cameraLayer.frame = screenSize;
+  //self.cameraLayer.bounds = rect;
+  //self.cameraLayer.masksToBounds = YES;
+	//self.cameraLayer.transform = CATransform3DRotate(CATransform3DIdentity, M_PI/2.0f, 0, 0, 1);
 	self.cameraLayer.contentsGravity = kCAGravityResizeAspectFill;
-	[self.layer addSublayer:self.cameraLayer];
+  
+  CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+  const CGFloat myColor[] = {1.0, 0, 0, 1.0};
+  CGColorRef c = CGColorCreate(rgb, myColor);
+  [self.cameraLayer setBackgroundColor: c];
+  CGColorSpaceRelease(rgb);
+  
+	//[self.layer addSublayer:self.cameraLayer];
 
 	[self.captureSession startRunning];
   
  	dispatch_release(queue);
 }
 
+-(CGImageRef) cropImage: (CGImageRef) img {
+	float width = CGImageGetWidth(img);
+  float height = CGImageGetHeight(img);
+	CGRect imgRect = CGRectMake(0,0, width/3, height);
+	return CGImageCreateWithImageInRect(img, imgRect);
+}
+
 - (void)captureOutput:(AVCaptureOutput *)captureOutput 
 didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer 
 	   fromConnection:(AVCaptureConnection *)connection 
 { 
-	/*We create an autorelease pool because as we are not in the main_queue our code is
-	 not executed in the main thread. So we have to create an autorelease pool for the thread we are in*/
-	
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	
-    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer); 
-    /*Lock the image buffer*/
-    CVPixelBufferLockBaseAddress(imageBuffer,0); 
-    /*Get information about the image*/
-    uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer); 
-    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer); 
-    size_t width = CVPixelBufferGetWidth(imageBuffer); 
-    size_t height = CVPixelBufferGetHeight(imageBuffer);  
+
+ 	CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer); 
+  CVPixelBufferLockBaseAddress(imageBuffer,0); 
+  uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddress(imageBuffer); 
+  size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer); 
+  size_t width = CVPixelBufferGetWidth(imageBuffer); 
+  size_t height = CVPixelBufferGetHeight(imageBuffer);  
     
-    /*Create a CGImageRef from the CVImageBufferRef*/
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB(); 
-    CGContextRef newContext = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
-    CGImageRef newImage = CGBitmapContextCreateImage(newContext); 
+  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB(); 
+  CGContextRef newContext = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+  CGImageRef newImage = CGBitmapContextCreateImage(newContext); 
 	
-    /*We release some components*/
-    CGContextRelease(newContext); 
-    CGColorSpaceRelease(colorSpace);
     
-    /*We display the result on the custom layer. All the display stuff must be done in the main thread because
-	 UIKit is no thread safe, and as we are not in the main thread (remember we didn't use the main_queue)
-	 we use performSelectorOnMainThread to call our CALayer and tell it to display the CGImage.*/
-	[self.cameraLayer performSelectorOnMainThread:@selector(setContents:) withObject: (id) newImage waitUntilDone:YES];
+	//[self.cameraLayer performSelectorOnMainThread:@selector(setContents:) withObject: (id) newImage waitUntilDone:YES];
 	
-	/*We display the result on the image view (We need to change the orientation of the image so that the video is displayed correctly).
-	 Same thing as for the CALayer we are not in the main thread so ...*/
-	UIImage *image= [UIImage imageWithCGImage:newImage scale:1.0 orientation:UIImageOrientationRight];
+  CGImageRef croppedImg = [self cropImage: newImage];
+	UIImage *image= [UIImage imageWithCGImage: croppedImg scale:1.0 orientation:UIImageOrientationRight];
 	
-	/*We relase the CGImageRef*/
-	CGImageRelease(newImage);
 	
 	[self.imageView performSelectorOnMainThread:@selector(setImage:) withObject:image waitUntilDone:YES];
 	
-	/*We unlock the  image buffer*/
 	CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+
+
+  CGContextRelease(newContext); 
+  CGColorSpaceRelease(colorSpace);
+	CGImageRelease(newImage);
+  CGImageRelease(croppedImg);
 	
 	[pool drain];
 } 
--- Classes/mainViewController.m
+++ Classes/mainViewController.m
@@ -15,6 +15,9 @@
 @synthesize wheelImage;
 @synthesize cameraView;
 
+// What portion of screen is dedicated to camera frame (i.e. 1/N of screen)
+#define CAMERA_FRAME_DIVIDER 3
+
 -(id) initWithCoder:(NSCoder *)aDecoder {
 	if (self = [super initWithCoder: aDecoder]) {
  		CGRect screenBounds = [[UIScreen mainScreen] bounds];
@@ -27,6 +30,7 @@
 -(id) init {
 	if (self = [super init]) {
  		CGRect screenBounds = [[UIScreen mainScreen] bounds];
+    screenBounds.size.height /= CAMERA_FRAME_DIVIDER;
   	cameraView *subview = [[[cameraView alloc] initWithFrame: screenBounds] autorelease];
     [subview initCapture];
   	[self setView: subview];
