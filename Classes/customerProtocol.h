9a3224647419aa1af5f6d0b33f8c3891e0f58602
mrmekon <none@none>
2011-04-30 01:02:56 -0400

    Added aitunesCustomer, implementation of the customerProtocol for AiTunes Karaoke Lounge.

--- Classes/customerProtocol.h
+++ Classes/customerProtocol.h
@@ -5,7 +5,36 @@
 //  Created by Trevor Bentley on 4/30/11.
 //  Copyright 2011 Trevor Bentley. All rights reserved.
 //
+//  This file is part of All-Seeing Eye.
+// 
+//  All-Seeing Eye is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  All-Seeing Eye is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with All-Seeing Eye.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * \brief Protocol for requesting customer data
+ *
+ * This file defines a protocol to be implemented by classes that provide data
+ * about a customer via database lookups.  A class that directly accesses the
+ * database should implement this protocol, and expect to be called by a
+ * view that handles displaying customer data.
+ *
+ * This is split out as a protocol to support the idea of deploying to multiple
+ * venues with minimal code change.  The database specifics and reward levels
+ * can change without changing the frontend view.  It does, however, limit the
+ * reward system to having levels, percent discounts, and monetary credits.
+ *
+ */
+ 
 #import <UIKit/UIKit.h>
 
 
