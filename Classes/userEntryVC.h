f5bea08e54edf7d3abef0fbe3bd4564c4982be55
mrmekon <none@none>
2011-05-17 21:59:09 -0400

    Started userEntryVC, screen for entering new customer info
    --HG--
    branch : administration_ui

---
+++ Classes/userEntryVC.h
@@ -0,0 +1,45 @@
+//
+//  userEntryVC.h
+//  All-Seeing Eye
+//
+//  Created by Trevor Bentley on 5/17/11.
+//  Copyright 2011 Trevor Bentley. All rights reserved.
+//
+//  This file is part of All-Seeing Eye.
+// 
+//  All-Seeing Eye is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  All-Seeing Eye is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with All-Seeing Eye.  If not, see <http://www.gnu.org/licenses/>.
+///\file
+
+#import <UIKit/UIKit.h>
+
+
+@interface userEntryVC : UITableViewController {
+	NSString *dbFile;
+
+}
+
+typedef enum {
+	SECTION_UNIQUE_ID,
+  SECTION_CONTACT_INFO,
+  SECTION_EXTRA_INFO,
+  SECTION_NOTES,
+  // ADD SECTIONS BEFORE THIS LINE
+  SECTION_COUNT,
+} AdjectiveVCSections;
+
+@property(nonatomic, retain) NSString *dbFile;
+
+- (id)initWithStyle:(UITableViewStyle)style withDbFile: (NSString*)db;
+
+@end
---
+++ Classes/userEntryVC.m
@@ -0,0 +1,117 @@
+//
+//  userEntryVC.m
+//  All-Seeing Eye
+//
+//  Created by Trevor Bentley on 5/17/11.
+//  Copyright 2011 Trevor Bentley. All rights reserved.
+//
+//  This file is part of All-Seeing Eye.
+// 
+//  All-Seeing Eye is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  All-Seeing Eye is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with All-Seeing Eye.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * \brief Customer entry form, for entering description of new customer
+ *
+ * 
+ */
+ 
+#import "userEntryVC.h"
+#import "mainAppDelegate.h"
+#import "databaseManager.h"
+
+
+@implementation userEntryVC
+
+@synthesize dbFile;
+
+- (id)initWithStyle:(UITableViewStyle)style withDbFile: (NSString*)db {
+  self = [super initWithStyle:style];
+  if (self) {
+      self.dbFile = db;
+  }
+  return self;
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return SECTION_COUNT;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+  switch (section) {
+  case SECTION_UNIQUE_ID:
+    return 2;
+  case SECTION_CONTACT_INFO:
+    return 6;
+  case SECTION_EXTRA_INFO:
+    return 2;
+  case SECTION_NOTES:
+  	return 1;
+  }
+  NSLog(@"What's this?  Unknown section!  Get out of here, monkey!");
+  return 0;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+    [self.navigationController pushViewController:detailViewController animated:YES];
+    [detailViewController release];
+    */
+}
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Relinquish ownership any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
+    // For example: self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
+
